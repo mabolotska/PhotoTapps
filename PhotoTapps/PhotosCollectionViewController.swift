f6bf48605dab6a874cdf951af4247220605e9d1b
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-20 15:22:20 +0200

    second screen

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -7,49 +7,56 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    let itemsPerRow: CGFloat = 3
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        
+//                let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//                layout.itemSize = CGSize(width: 70, height: 70)
+//                layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//                layout.minimumLineSpacing = 1
+//                layout.minimumInteritemSpacing = 1
+//                layout.scrollDirection = .vertical
+//        
+//                collectionView.showsVerticalScrollIndicator = false
+     
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "pickPhotoSegue" {
+            let photoVC = segue.destination as! ViewController
+            let cell = sender as! PhotoCell
+            photoVC.image = cell.dogImageView.image
+        }
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+  
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.dogImageView.image = image
     
         return cell
     }
@@ -85,4 +92,31 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
     */
 
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        let padding = sectionInserts.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - padding
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInserts
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInserts.left
+        }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInserts.left
+        }
+    }
 }
--- PhotoTapps/ViewController.swift
+++ PhotoTapps/ViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var image: UIImage?
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        photoImageView.image = image
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func share(_ sender: UIButton) {
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("Успешно!")
+            }
+        }
+        
+        present(shareController, animated: true, completion: nil)
     }
-    */
-
+    
 }
